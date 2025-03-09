8adbf14a067c67a1dd008f1b2983c039a99c2b31
Anton Popov <mffnrscl@gmail.com>
2025-03-09 12:52:12 +0200

    done task4

---
+++ lab1/cs/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab
+{
+    public delegate double Func(int x);
+    public static class Program
+    {
+        public static void Main()
+        {
+            Func[] funcs = new Func[3];
+
+            funcs[0] = x => Math.Sqrt(Math.Abs(x));
+            funcs[1] = x => x * x * x;
+            funcs[2] = x => x + 3.5;
+
+            while (true)
+            {
+                try
+                {
+                    string[] read = Console.ReadLine().Trim().Split();
+                    int choise = int.Parse(read[0]);
+                    int val = int.Parse(read[1]);
+
+                    Func pickedFunc = funcs[choise];
+
+                    Console.WriteLine(pickedFunc(val));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Up From the Skies.");
+                    break;
+                }
+            }
+        }
+    }
+}
