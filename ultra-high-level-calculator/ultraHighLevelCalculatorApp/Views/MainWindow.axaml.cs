6a0bfd43dfc56d2f202570e17bf8c0a1e602c489
Anton Popov <mffnrscl@gmail.com>
2025-03-16 18:47:05 +0200

    created lab2 super mega calculator

---
+++ ultra-high-level-calculator/ultraHighLevelCalculatorApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ultraHighLevelCalculatorApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
